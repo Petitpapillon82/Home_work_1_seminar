782c601de0d40be74ca4ea3a270363e823e248a1
Irina <petitpapillon82@gmail.com>
2022-08-21 02:50:01 +0400

    Add HW_task3 (even or odd number)

---
+++ HW_task3/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine()); 
+int x = number;
+if (x%2==0)
+{
+    Console.WriteLine($"число {x} чётное");
+}
+else
+{
+    Console.WriteLine($"число {x} нечётное");
+}
