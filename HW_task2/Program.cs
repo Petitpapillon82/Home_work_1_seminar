eca3aa9a0347e8fdbf82b7aa36149375fa40c793
Irina <petitpapillon82@gmail.com>
2022-08-21 02:34:42 +0400

    Add HW_task2 (max for 3 numbers)

---
+++ HW_task2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.Write("Введите первое число: ");
+int number1 = int.Parse(Console.ReadLine()); 
+Console.Write("Введите второе число: ");
+int number2 = int.Parse(Console.ReadLine()); 
+Console.Write("Введите третье число: ");
+int number3 = int.Parse(Console.ReadLine()); 
+
+int max = number1;
+if (number2 > number1) max = number2;
+if (number3 > number2) max = number3;
+ Console.WriteLine(max);
