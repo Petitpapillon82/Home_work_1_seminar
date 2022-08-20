a291836ca3f79224f37ecce93f7c57f50c103ee1
Irina <petitpapillon82@gmail.com>
2022-08-21 02:20:46 +0400

    Add HW_task1 (max and min for two numbers)

---
+++ HW_task1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.Write("Введите первое число: ");
+int number1 = int.Parse(Console.ReadLine()); 
+Console.Write("Введите второе число: ");
+int number2 = int.Parse(Console.ReadLine()); 
+
+if (number1 > number2) 
+{
+    Console.WriteLine($"max == {number1}, min == {number2}");
+}
+else 
+{
+    Console.WriteLine($"max == {number2}, min == {number1}");
+}
